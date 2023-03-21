44d1b82d585902e8d0946209145943b78c9e2f4c
eva-gevorgyan <127292682+eva-gevorgyan@users.noreply.github.com>
2023-03-21 11:43:59 +0400

    Add files via upload

---
+++ DelEvent/DelEvent/BankCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelEvent
+{
+    internal class BankCard
+    {
+        public int Balance { get; set; }
+        public BankCard(int Balance)
+        {
+            this.Balance = Balance;
+        }
+        public void Add(int sum)
+        {
+            Message.Invoke($"+ {sum}");
+            Balance += sum;
+            Message.Invoke($"Your balance - {Balance}");
+        }
+        public void Remove(int sum)
+        {
+            if (Balance > sum)
+            {
+                Message.Invoke($"- {sum}");
+                Balance -= sum;
+                Message.Invoke($"Your balance - {Balance}");
+            }
+            else
+            {
+                Message.Invoke("Not enough");
+                Message.Invoke($"Your balance - {Balance}");
+            }
+        }
+        public delegate void MyDelegate(string message);
+        public event MyDelegate Message;
+    }
+}
---
+++ DelEvent/DelEvent/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DelEvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BankCard bankCard = new BankCard(150);
+            //Console.WriteLine($"{bankCard.Balance}");    
+            //bankCard.Add(50);
+            //Console.WriteLine($" Add {bankCard.Balance}");
+            //bankCard.Remove(50);
+            //Console.WriteLine($" Remove {bankCard.Balance}");
+            bankCard.Message += showMessage;
+            bankCard.Add(50);
+            bankCard.Remove(450);
+            static void showMessage(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
